0829784aab3b315302ee48863eb9d469cbc1b134
Mashchenko <dremka@hotmail.com>
2022-06-30 19:33:18 +0200

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,24 +1,35 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../components/layout'
+import {IData, IMdx, INodes} from "../types/data";
 
-const BlogPage: React.FC<{
-    data:{
-        allFile: {
-            nodes: Array<{
-                name: string
-            }>
-        }
-    }
-}> = ({data}) => {
+interface IBlog {
+    id: string;
+    body: string;
+    frontmatter: {
+        title: string;
+        name: string;
+        author: string;
+        date: string;
+    };
+}
+
+interface IBlogPageProps extends IData<IMdx<INodes<IBlog>>>{}
+
+const BlogPage: React.FC<IBlogPageProps> = ({data}) => {
     return (
         <Layout pageTitle="My Blog Posts">
             <ul>
                 {
-                    data.allFile.nodes.map(node => (
-                        <li key={node.name}>
-                            {node.name}
-                        </li>
+                    data.allMdx.nodes.map((node) => (
+                        <article key={node.id}>
+                            <h2>{node.frontmatter.title}</h2>
+                            <p>Posted: {node.frontmatter.date}</p>
+                            <MDXRenderer>
+                                {node.body}
+                            </MDXRenderer>
+                        </article>
                     ))
                 }
             </ul>
@@ -28,11 +39,18 @@ const BlogPage: React.FC<{
 
 export const query = graphql`
   query {
-    allFile {
-      nodes {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+    nodes {
+      frontmatter {
+        title
         name
+        author
+        date(formatString: "MMMM D, YYYY")
       }
+      id
+      body
     }
+  }
   }
 `
 
---
+++ src/types/data.ts
@@ -0,0 +1,11 @@
+export interface IData<T> {
+    data: T
+}
+
+export interface IMdx<T> {
+    allMdx: T
+}
+
+export interface INodes<T> {
+    nodes: T[]
+}
